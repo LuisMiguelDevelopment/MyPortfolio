b0d03558d2e75d5f430b9ef1889caafdff0d94b4
Luis Miguel <luismiguealvarezmarin109@gmail.com>
2024-06-21 01:44:34 -0500

    feat: :sparkles: New project add

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -2,8 +2,8 @@ import './projects.css';
 import Title from '../../components/atoms/Title/Title';
 import Card from '../../components/molecules/Card/Card';
 import Licorera from '../../assets/img/Licorerala69.jpg'
-import Studio from '../../assets/img/StudioGhibli.jpg'
-import Login from '../../assets/img/LoginMEAN.jpg'
+import VisionWeb from '../../assets/img/VISIONWEB.png'
+import MyPortfolio from '../../assets/img/MyPortfolio.png'
 import Layout from "../../_config/Layout"
 
 
@@ -14,9 +14,9 @@ const Projects = () => {
         <Title text={'Projects'} level={'h3'} />
       </div>
       <div className="card-projects">
+        <Card link={'https://github.com/LuisMiguelDevelopment/VISIONWEB.git'} image={VisionWeb} alt={'image'}  name={'Vision Web'} showButton={true} icon={'github'}  description={'Vision web is a social network of video calls which you can interact with friends, it was made with Socket.io , Simple-peer , sql server , nodejs and Nextjs.'} />
+        <Card link={'https://github.com/LuisMiguelDevelopment/MyPortfolio.git'} image={MyPortfolio} alt={'image'} name={'My Portfolio'} showButton={true} icon={'github'}  description={'My portfolio was made with Vitejs, Three.js, a 3D library for JavaScript and Framer Motion an animation library and using atomic design.'} />
         <Card link={'https://github.com/LuisMiguelDevelopment/LicoreraLa69.git'} image={Licorera} alt={'image'} showButton={true} icon={'github'}   name={'Licorera la 69'} description={'Licorera la 69 is a shopping cart web application for a liquor store in itagui antioquia. This was the project done in the SENA.'} />
-        <Card link={'https://github.com/LuisMiguelDevelopment/StudioGhibli.git'} image={Studio} alt={'image'}  name={'Studio Ghibli'} showButton={true} icon={'github'}  description={'Studio ghibli is a portfolio of images made using flex is a work done for the SENA.'} />
-        <Card link={'https://github.com/LuisMiguelDevelopment/CRUD-18-07-2023.git'} image={Login} alt={'image'} name={'Login Mean'} showButton={true} icon={'github'}  description={'Login Mean is a full stack work done with angular and a node js server for SENA.'} />
       </div>
     </Layout>
   )
